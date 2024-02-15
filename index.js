5f4790031c3053b6a9438211c214ebd3ac4c9aef
eveporcello <eve@moonhighway.com>
2024-02-15 14:12:37 -0800

    04_01e

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import "dotenv/config";
 import axios from "axios";
 
-axios
-  .post(
-    "https://api.openai.com/v1/images/generations",
-    {
-      model: "dall-e-3",
-      prompt:
-        "Illustrate a majestic and confident-looking horse, standing firmly with its head held high. Capture the essence of the horse's bravery and self-assurance in its posture, its bright eyes and spirited expression. Make sure the horse's muscular and athletic structure radiates in the sunlight. The setting can be a lush green meadow with a calm blue sky overhead. Include few distant trees and birds in the sky adding to the serene and calm environment. The horse should be a symbol of strength and confidence in this peaceful setting.",
-      size: "1024x1024"
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        "Content-Type": `application/json`
+const generateImage = async (prompt, size) => {
+  try {
+    const openAIAPI =
+      "https://api.openai.com/v1/images/generations";
+    const response = await axios.post(
+      openAIAPI,
+      { model: "dall-e-3", prompt, size },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": `application/json`
+        }
       }
-    }
-  )
-  .then((response) => console.log(response.data));
+    );
+    return response.data;
+  } catch (error) {
+    console.error(
+      `Error occurred while generating image: ${error}`
+    );
+  }
+};
+
+generateImage(
+  "Illustrate a majestic and confident-looking horse, standing firmly with its head held high...",
+  "1024x1024"
+).then((data) => console.log(data));

c6477c84e8c61f1f9a68d4d7a137ca44386a4feb
eveporcello <eve@moonhighway.com>
2024-02-15 13:57:58 -0800

    Adds 03_01e

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 import OpenAI from "openai";
+import readline from "readline";
 
 const openai = new OpenAI();
 
-// Create a prompt that generates questions for a job interview
-// Ask for 3 interview questions for a JavaScript Developer
-// Extra Credit: Make this a template to make this dynamic for any kind of developer
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-async function hello(language) {
-  const completion = await openai.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content:
-          "You will be asked to create job interview questions."
-      },
-      {
-        role: "user",
-        content: `Give me 3 interview questions for a ${language} developer.`
-      }
-    ],
-    model: "gpt-3.5-turbo"
-  });
-  console.log(completion.choices[0].message.content);
-}
-
-hello("Rust");
+rl.question(
+  "What do you want to ask the robots",
+  async (question) => {
+    let res = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: "You are a friendly robot!"
+        },
+        {
+          role: "user",
+          content: question
+        }
+      ],
+      model: "gpt-3.5-turbo"
+    });
+    console.log(res.choices[0].message);
+    rl.close();
+  }
+);

45de0a385e6e685cb9b1282c0708bd7ba7444ca1
eveporcello <eve@moonhighway.com>
2024-02-15 13:09:30 -0800

    Adds 01_03e

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ import OpenAI from "openai";
 
 const openai = new OpenAI();
 
-console.log(openai);
+async function hello() {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "You are a great robot!" }
+    ],
+    model: "gpt-3.5-turbo"
+  });
+  console.log(completion.choices[0]);
+}
+
+hello();

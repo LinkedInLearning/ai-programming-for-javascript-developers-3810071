847fb70d22c7163f316aa427e2ebfa0132536174
eveporcello <eve@moonhighway.com>
2024-02-15 13:41:38 -0800

    Adds 02_01b

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const openai = new OpenAI();
 async function hello() {
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: "system", content: "You are a great robot!" },
-      {
-        role: "user",
-        content:
-          "What is the closest ski resort to Boise, Idaho?"
-      }
+      { role: "system", content: "You are a great robot!" }
     ],
     model: "gpt-3.5-turbo"
   });

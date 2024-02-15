f14bddce68da42fd6be07d67affe8d81edc532db
eveporcello <eve@moonhighway.com>
2024-02-15 13:46:30 -0800

    Adds 02_03e

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const openai = new OpenAI();
 async function hello() {
   const stream = await openai.chat.completions.create({
     messages: [
-      { role: "system", content: "You are a great robot!" },
+      {
+        role: "system",
+        content:
+          "You are a motivational speaker who is encouraging me as a JavaScript developer to keep studying and doing the hard work necessary for success."
+      },
       {
         role: "user",
         content:
-          "What is the closest ski resort to Boise, Idaho?"
+          "What do I need to study to be a great JavaScript developer who takes advantage of the latest techniques in AI?"
       }
     ],
     model: "gpt-3.5-turbo",
-    stream: true
+    stream: true,
+    max_tokens: 60
   });
   for await (const chunk of stream) {
     process.stdout.write(

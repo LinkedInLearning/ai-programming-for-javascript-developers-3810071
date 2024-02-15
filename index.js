72a84fd67b7fa2a969c556eb7fb1ddd3b3b02107
eveporcello <eve@moonhighway.com>
2024-02-15 13:54:07 -0800

    Adds 02_07e

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const openai = new OpenAI();
 // Ask for 3 interview questions for a JavaScript Developer
 // Extra Credit: Make this a template to make this dynamic for any kind of developer
 
-async function hello() {}
+async function hello(language) {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content:
+          "You will be asked to create job interview questions."
+      },
+      {
+        role: "user",
+        content: `Give me 3 interview questions for a ${language} developer.`
+      }
+    ],
+    model: "gpt-3.5-turbo"
+  });
+  console.log(completion.choices[0].message.content);
+}
 
-hello();
+hello("Rust");

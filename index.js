4733f0b5332151c6bed9099486b93d91ffe90e72
eveporcello <eve@moonhighway.com>
2024-02-15 14:24:42 -0800

    Adds 05_04e

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ import "dotenv/config";
 
 const chatModel = new ChatOpenAI();
 
-const prompt = PromptTemplate.fromTemplate(
-  "Write a haiku about {topic}"
+const prompt1 = PromptTemplate.fromTemplate(
+  "What town is {restaurant} restaurant in? Respond with the name of one town."
+);
+
+const prompt2 = PromptTemplate.fromTemplate(
+  "what country is the restaurant {restaurant} in? Respond in {language}."
 );
 
 const parser = new StringOutputParser();
 
-const chain = RunnableSequence.from([
-  prompt,
+const chain = prompt1.pipe(chatModel).pipe(parser);
+
+const bigChain = RunnableSequence.from([
+  {
+    restaurant: chain,
+    language: (input) => input.language
+  },
+  prompt2,
   chatModel,
   parser
 ]);
 
-const response = await chain.invoke({
-  topic: "spaceships"
+const response = await bigChain.invoke({
+  restaurant: "Le Pigeon",
+  language: "French"
 });
 
 console.log(response);
